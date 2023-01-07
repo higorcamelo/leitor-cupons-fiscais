7c9570033745203a0bc9b3de957b03907b7a5583
Higor Camelo <higorcamelo@gmail.com>
2023-01-07 19:06:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from pytesseract import pytesseract
+import csv
 import cv2
 import re
 import PySimpleGUI as sg
@@ -10,27 +11,40 @@ class Janela():
     def __init__(self):
         #sg.Theme('Reddit')
         layout = [
-            [sg.Text('Selecione uma imagem:')],
-            [sg.InputText(key='file_path'),
+            [sg.Text('Selecione uma imagem:'), sg.InputText(key='file_path'),
             sg.FileBrowse(target = 'file_path', initial_folder = diretorio, file_types =[('Arquivos de imagem', '.png .jpeg')]),
-            sg.Button('Importar'),
-            sg.Output(size = (100,40))
-            ]
+            sg.Button('Importar')],
+            [sg.Text('Nomeie seu arquivo:'), sg.InputText(key='nome_arquivo')],
+            [sg.Button('Confirmar e exportar', key = 'confirmar')],
+            #sg.Output(size = (50,10))
         ]
         self.criarJanela = sg.Window('Leitor de Cupons Fiscais').layout(layout)
 
     def leitorImagem(self, endereco_imagem):
-        dict_itens = dict.fromkeys(['desc', 'qtd', 'valor'])
+        dict_itens = dict.fromkeys(['Descricao', 'Quantidade', 'Valor'])
         vetor_nomes = []
         regex = re.compile(r'^[0-9]+\s+[0-9]+\s+((\S+(?:\s*\S*)*?)\s+([0-9]+(?:,[0-9]+)?)\s+\w\w)\s+.*\s+([0-9]+(?:,[0-9]+)?)$', flags=re.M)
         texto_imagem = pytesseract.image_to_string(cv2.imread(endereco_imagem))
 
         for match in regex.finditer(texto_imagem):
-            print("teste")
-            vetor_nomes.append(match.group(3))
+            dict_itens['Descricao'] = match.group(2)
+            dict_itens['Quantidade'] = match.group(3)
+            dict_itens['Valor'] = match.group(4)
+            
+            vetor_nomes.append(dict_itens.copy())
 
         return vetor_nomes
 
+    def criar_csv(self, nome_arquivo, vetor_nomes):
+        if(nome_arquivo == None):
+            nome_arquivo = 'meuarquivo'
+        arquivo_csv = open('${nome_arquivo}.csv', 'w')
+        with open('${nome_arquivo}.csv','w', newline = '') as arquivo:
+            escreve = csv.DictWriter(arquivo, fieldnames = vetor_nomes[0].keys())
+            escreve.writeheader()
+            escreve.writerows(vetor_nomes)
+
+        return True
 
     def Iniciar(self):
         while True:
@@ -41,7 +55,14 @@ class Janela():
             if self.evento == 'Importar':
                 print(self.values['file_path'])
                 endereco_imagem = self.values['file_path']
-                print(self.leitorImagem(endereco_imagem))
+                dados_itens_temp = self.leitorImagem(endereco_imagem)
+                dados_itens = dados_itens + dados_itens_temp
+                print(dados_itens)
+
+            if self.evento == 'confirmar':
+                self.criar_csv(self.values['nome_arquivo'], self.dados_itens)
+                if(self.leitorImagem(endereco_imagem) == True):
+                    print('Operação realizada com sucesso!')
 
 tela = Janela()
 tela.Iniciar()
